36d02365fa4029f288f257f49d363f1eb15e153e
adhamy-4 <adwojewski4@gmail.com>
2025-04-11 13:30:07 -0400

    Updated HelloApplication.java

--- HelloApplication.java
+++ HelloApplication.java
@@ -1,6 +1,7 @@
 package org.example.chatroom;
 
 import javafx.application.Application;
+import javafx.css.PseudoClass;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -49,18 +50,18 @@ public class HelloApplication extends Application {
         portBox = new TextField();
         gridConnect.add(portBox, 1, 3);
 
-        Button b = new Button("Submit");
-        b.setOnAction(event -> {
+        Button submitB = new Button("Submit");
+        submitB.setOnAction(event -> {
             String displayNameText = userTextField.getText();
             String ipAddress = ipBox.getText();
             String port = portBox.getText();
 
             // validate input
             if ( displayNameText.isEmpty() || ipAddress.isEmpty() || port.isEmpty() ) {
-                alert("Empty Box", "Please do not leave anything blank.");// look into alerts, maybe add function
+                alert("Empty Box", "Please do not leave anything blank.");
             } else {
                 if (!portValidation(port)) {
-                    alert("Invalid input", "Please enter a valid port number."); // another alert
+                    alert("Invalid input", "Please enter a valid port number.");
                 } else {
                     System.out.println("Connecting " + displayNameText + " to " + ipAddress + ":" + port);
                     try {
@@ -72,7 +73,7 @@ public class HelloApplication extends Application {
             }
         });
 
-        gridConnect.add(b, 1, 4);
+        gridConnect.add(submitB, 1, 4);
 
         Scene sceneMain = new Scene(gridConnect, 300, 275);
         stage.setScene(sceneMain);
@@ -84,24 +85,47 @@ public class HelloApplication extends Application {
         String displayNameText = userTextField.getText();
         String ipAddress = ipBox.getText();
         String port = portBox.getText();
-
-        GridPane gridChat = new GridPane();
         stage.setTitle("Welcome to the chat room!");
-        gridChat.setAlignment(Pos.TOP_RIGHT);
-        gridChat.setHgap(10);
-        gridChat.setVgap(10);
-        gridChat.setPadding(new Insets(25, 25, 25, 25));
 
-        Label IP = new Label("IP: " + ipAddress);
-        gridChat.add(IP, 0, 0);
+        BorderPane borderPane = new BorderPane();
 
-        Label Port = new Label("Port: " + port);
-        gridChat.add(Port, 1, 0);
+        Label inputInfo = new Label("Display Name: " + displayNameText + " | IP Address: " + ipAddress + " | Port: " + port);
+        HBox infoBox = new HBox(inputInfo);
+        infoBox.setPadding(new Insets(10));
+        infoBox.setAlignment(Pos.TOP_RIGHT);
 
-        Label Name = new Label("Display Name: " + displayNameText);
-        gridChat.add(Name, 2, 0);
+        Label Users = new Label("Users");
+        Label activePeople = new Label("Active People: ");
+        //also display the active users in a new label here
+        VBox usersBox = new VBox(Users, activePeople);
+        usersBox.setPadding(new Insets(25));
+        usersBox.setAlignment(Pos.TOP_CENTER);
 
-        Scene sceneChat = new Scene(gridChat, 500, 500);
+        TextArea chatBox = new TextArea();
+        chatBox.setEditable(false);
+        chatBox.setWrapText(true);
+        chatBox.setStyle("-fx-background-color: white;");
+
+        TextField sendBox = new TextField();
+        sendBox.setPromptText("Message...");
+        Button sendB = new Button("Send");
+        sendB.setOnAction(event -> {
+
+        });
+
+        PseudoClass empty = PseudoClass.getPseudoClass("empty");
+        sendBox.pseudoClassStateChanged(empty, true);
+        sendBox.textProperty().addListener((obs, oldText, newText) -> {
+            sendBox.pseudoClassStateChanged(empty, newText.isEmpty());
+        });
+
+
+        borderPane.setTop(infoBox);
+        borderPane.setLeft(usersBox);
+        borderPane.setCenter(chatBox);
+        borderPane.setBottom(sendBox);
+
+        Scene sceneChat = new Scene(borderPane, 500, 500);
         stage.setScene(sceneChat);
         stage.show();
 
