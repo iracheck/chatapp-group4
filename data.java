23528df9d14b1b4bbb14bca7c00962e8123f1ad0
Kyle Jones <55156949+kj000058@users.noreply.github.com>
2025-04-29 12:59:45 -0400

    Remove Intelij imports

---
+++ data.java
@@ -0,0 +1,7 @@
+
+
+public class data {
+    static volatile String[] CLIENT_INFO = new String[]{"-1", "-1", "-1"};
+    static volatile String inMessages;
+    static volatile String outMessages;
+}
