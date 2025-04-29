be8f6170872e860e7483423fcddb45f8f944b656
Kyle Jones <55156949+kj000058@users.noreply.github.com>
2025-04-29 13:29:55 -0400

    Delete data.java

--- data.java
+++
@@ -1,7 +0,0 @@
-
-
-public class data {
-    static volatile String[] CLIENT_INFO = new String[]{"-1", "-1", "-1"};
-    static volatile String inMessages;
-    static volatile String outMessages;
-}
